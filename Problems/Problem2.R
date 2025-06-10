adc2e8733ec539d72754863f006b90fcaa8fdd1d
k3ttyy <128756030+ok3tty@users.noreply.github.com>
2025-06-10 05:14:00 +0000

    Did problem2 need to review

--- Problems/Problem2.R
+++ Problems/Problem2.R
@@ -1,8 +1,9 @@
 library(shiny)
 library(DT)
 
-ur <- fluidPage(
-    DTOutput("table")
+ui <- fluidPage(
+    DTOutput("table"),
+    verbatimTextOutput("contingency_table")  # Add this to show contingency table
 )
 
 server <- function(input, output) {
@@ -11,5 +12,43 @@ server <- function(input, output) {
     output$table <- renderDT({
         datatable(track)
     })
+
+    output$contingency_table <- renderPrint({
+
+        # (a) Construct a contingency table for FacTeaching and COL
+        contingency_table <- table(track$FacTeaching, track$COL)
+        print(contingency_table)
+
+        # (b) Construct a proportion table
+        prop_table <- prop.table(contingency_table)
+        print(prop_table)
+
+        # (c) Probability of being from College of Education (ED) and rating instructors at 4
+        prob_ED_4 <- prop_table["4", "ED"]
+        cat("Probability of being from College of Education (ED) and rating instructors at 4:", prob_ED_4, "\n")
+
+        # (d) Probability of being from College of Business Administration (BA)
+        prob_BA <- sum(prop_table[, "BA"])
+        cat("Probability of being from College of Business Administration (BA):", prob_BA, "\n")
+
+        # (e) Probability of rating instructors at a value of 2
+        prob_rating_2 <- sum(prop_table["2", ])
+        cat("Probability of rating instructors at a value of 2:", prob_rating_2, "\n")
+
+        # (f) Conditional probability given College of Arts and Sciences (AS) and rating at 3
+        cond_prob_AS_3 <- prop_table["3", "AS"] / sum(prop_table[, "AS"])
+        cat("Conditional probability given College of Arts and Sciences (AS) and rating at 3:", cond_prob_AS_3, "\n")
+
+        # (g) Conditional probability given rating at 5 and College of Visual and Performing Arts (VPA)
+        cond_prob_VPA_5 <- prop_table["5", "VPA"] / sum(prop_table["5", ])
+        cat("Conditional probability given rating at 5 and College of Visual and Performing Arts (VPA):", cond_prob_VPA_5, "\n")
+
+        # (h) Assess quality of teaching between BA and ED
+        ba_ed_quality <- prop.table(contingency_table[, c("BA", "ED")])
+        print(ba_ed_quality)
+    })
     
 }
+
+
+shinyApp(ui, server)
